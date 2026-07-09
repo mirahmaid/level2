071d40f0f067ce10548fdfd4e0ee859f0e2631cd
mira hmaid <mira.akram2025@gmail.com>
2026-07-09 12:49:52 +0300

    Add lesson 3 and 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-
 import './App.css'
+import { useState } from "react";
 
 function App() {
+    const [feild, setFeild] = useState("React");
+    const [level, setLevel] = useState("senior");
+const [count, setCount] = useState(0);
 
-  return (
+    return (
     <>
-    
-          <h1>Vite + react</h1>
-        
+      <h1>My field is {feild}</h1>
+      <button   onClick={() => setFeild("Angular")}
+>Change field</button>
+<br>
+</br>
+<h1>
+  I'm a {level}
+</h1>
+<button onClick={() => {setLevel("junior")}}>Change Level</button>
+  <br>
+</br>
+  <button onClick={() => { setCount(count+1) }}>Counter{count}</button>
     </>
-  )
+  ) 
 }
 
 export default App
