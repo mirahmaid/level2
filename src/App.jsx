0d7763afc7f7114890ec305e1f7a38740cd14c39
mira hmaid <mira.akram2025@gmail.com>
2026-07-09 16:41:32 +0300

    Add lesson5

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
-import './App.css'
+import "./App.css";
+import "./Theme.css";
+
 import { useState } from "react";
 
 function App() {
-    const [feild, setFeild] = useState("React");
-    const [level, setLevel] = useState("senior");
-const [count, setCount] = useState(0);
+  const [feild, setFeild] = useState("React");
+  const [level, setLevel] = useState("senior");
+  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState("");
 
-    return (
-    <>
+  return (
+    <div className={`App ${theme}`}>
       <h1>My field is {feild}</h1>
-      <button   onClick={() => setFeild("Angular")}
->Change field</button>
-<br>
-</br>
-<h1>
-  I'm a {level}
-</h1>
-<button onClick={() => {setLevel("junior")}}>Change Level</button>
-  <br>
-</br>
-  <button onClick={() => { setCount(count+1) }}>Counter{count}</button>
-    </>
-  ) 
+      <button onClick={() => setFeild("Angular")}>Change field</button>
+      <br/>
+      <h1>I'm a {level}</h1>
+      <button onClick={() => setLevel("junior")}>Change Level</button>
+      <br/>
+      <button onClick={() => setCount(count+1)}>Counter {count}</button>
+      <br/>
+      <br/>
+    <div>
+        <button onClick={() => setTheme("dark")} style={{ marginRight: "20px" }}>  Dark  </button>
+        <button onClick={() => setTheme("light")}style={{ marginRight: "20px" }}>  Light  </button>
+        <button onClick={() => setTheme("pink")} style={{ marginRight: "20px" }}>  Pink  </button>
+    </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
