ba0ca2eb7b0eaa1306f5dc2b36d813f8d7586985
mira hmaid <mira.akram2025@gmail.com>
2026-07-10 16:36:35 +0300

    Add lesson8

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,71 @@
 import "./App.css";
 import "./Theme.css";
-import { FaSun ,FaMoon  } from "react-icons/fa";
-
-import { useState } from "react";
+import { FaSun, FaMoon } from "react-icons/fa";
+import { useReducer } from "react";
+const initialData = {feild:"React.js",level:"senior",count:0,theme:"light"}
+const reducer = (state, action) => {
+   switch (action.type) {
+    case "CHANGE_FEILD":
+    return {...state, feild:action.newValue };
+   case "CHANGE_LEVEL": return { ...state,level: action.newValue};
+       case "INCREASE":
+      return {...state, count: state.count+1};
+    case "CHANGE_THEME":return {...state,  theme: action.newValue};
+    default:
+      return state;
+  } 
+};
 
 function App() {
-  const [feild, setFeild] = useState("React");
-  const [level, setLevel] = useState("senior");
-  const [count, setCount] = useState(0);
-  const [theme, setTheme] = useState("");
+  const [Data, dispatch] = useReducer(reducer, initialData);
 
   return (
-    <div className={`App ${theme}`}>
-      <h1>My field is {feild}</h1>
-      <button onClick={() => setFeild("Angular")}>Change field</button>
-      <br/>
-      <h1>I'm a {level}</h1>
-      <button onClick={() => setLevel("junior")}>Change Level</button>
-      <br/>
-      <button onClick={() => setCount(count+1)}>Counter {count}</button>
-      <br/>
-      <br/>
-    <div>
-        <button onClick={() => setTheme("dark")} style={{ marginRight: "20px" }}>  Dark  </button>
-        <button onClick={() => setTheme("light")}style={{ marginRight: "20px" }}>  Light  </button>
-        <button onClick={() => setTheme("pink")} style={{ marginRight: "20px" }}>  Pink  </button>
-    </div>
-    <div>
-      <button onClick={() => setTheme(theme==="dark"? "light":"dark")}
-       style = {{marginTop:"30px"}}>
-        {theme ==="dark"?<FaSun />:<FaMoon />}
-
-       </button>
-    </div>
+    <div className={`App ${Data.theme}`}>
+      <h1>My field is {Data.feild} </h1>
+      <button onClick={() => {
+            dispatch({ type: "CHANGE_FEILD", newValue:"Angular"});
+        
+      }
+      }>Change field</button>
+      <br />
+      <h1>I'm a {Data.level}</h1>
+      <button onClick={() => {
+            dispatch({ type: "CHANGE_LEVEL", newValue:"junior"});
+        
+      }
+      }>Change Level </button>
+      <br />
+      <button onClick={() => {
+            dispatch({ type: "INCREASE"});
+        
+      }
+      }>Counter {Data.count}</button>
+      <br />
+      <br />
+      <div>
+        <button onClick={() => {
+            dispatch({ type: "CHANGE_THEME", newValue:"dark"});
+        
+      }
+      }style={{ marginRight: "20px" }}> Dark </button>
+        <button onClick={() => {
+            dispatch({ type: "CHANGE_THEME", newValue:"light"});
+        
+      }
+      }style={{ marginRight: "20px" }}> Light </button>
+        <button onClick={() => {
+            dispatch({ type: "CHANGE_THEME", newValue:"pink"});
+        
+      }
+      }style={{ marginRight: "20px" }}> Pink </button>
+      </div>
+      <div>
+        <button style={{ marginTop: "30px" }}>
+          <FaSun />:<FaMoon />
+        </button>
+      </div>
     </div>
   );
 }
+
 export default App;
